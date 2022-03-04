61239b2a5d570503e8366993acf4db988ed20933
Top Coder <61309193+topcoder1208@users.noreply.github.com>
2022-03-04 09:44:23 +0300

    updated

--- demo/start_rpc.sh
+++ demo/start_rpc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-NODE_PATH="/root/mugambo/nodes"
-LOG_PATH="/root/mugambo/nodes/logs"
+NODE_PATH="/root/Fork/mugambo-foundation/nodes"
+LOG_PATH="/root/Fork/mugambo-foundation/nodes/logs"
+GENESIS_PATH="/root/genesis"
 
-"$NODE_PATH"/mugambo --datadir="$NODE_PATH"/rpc --genesis="$NODE_PATH"/mainnet.g --port=9009 --nat extip:0.0.0.0 --http --http.addr="0.0.0.0" --http.port=80 --http.corsdomain="*" --http.api="eth,debug,net,admin,web3,personal,txpool,znx,dag" --ws --ws.addr="0.0.0.0" --ws.port=81 --ws.origins="*" --ws.api="eth,debug,net,admin,web3,personal,txpool,znx,dag" --nousb --verbosity=3 --tracing >> "$LOG_PATH"/rpc.log --ipcpath=\\.\pipe\rpc.ipc
+"$NODE_PATH"/mugambo --datadir="$NODE_PATH"/rpc --genesis="$GENESIS_PATH"/testnet.g --port=9009 --nat extip:0.0.0.0 --http --http.addr="0.0.0.0" --http.port=80 --http.corsdomain="*" --http.api="eth,debug,net,admin,web3,personal,txpool,mgb,dag" --ws --ws.addr="0.0.0.0" --ws.port=81 --ws.origins="*" --ws.api="eth,debug,net,admin,web3,personal,txpool,mgb,dag" --nousb --verbosity=3 --tracing >> "$LOG_PATH"/rpc.log --ipcpath=\\.\pipe\rpc.ipc
